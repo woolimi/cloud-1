dce45d058b8caf04cddb3ca307f79a8d62f193cf
Woolim Park <woolimi91@naver.com>
2024-10-02 13:59:58 +0200

    WIP wordpress

--- roles/inception/files/wordpress/init.sh
+++ roles/inception/files/wordpress/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sed -i "s|WP_DOMAIN|$WP_DOMAIN|" /var/www/html/wp-config.php
-sed -i "s|WP_DB_PASSWORD|$MYSQL_WP_PASSWORD|" /var/www/html/wp-config.php
+sed -i "s|WP_DOMAIN|$WP_DOMAIN|" /var/www/html/wordpress/wp-config.php
+sed -i "s|WP_DB_PASSWORD|$MYSQL_WP_PASSWORD|" /var/www/html/wordpress/wp-config.php
 
 php-fpm7 -F -R
