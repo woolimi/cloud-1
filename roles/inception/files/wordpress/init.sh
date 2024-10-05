9f89b7f559d9011f37481bc97d996e4b28fe45ce
Woolim Park <woolimi91@naver.com>
2024-10-05 14:37:46 +0200

    WIP

--- roles/inception/files/wordpress/init.sh
+++ roles/inception/files/wordpress/init.sh
@@ -2,4 +2,4 @@
 sed -i "s|WP_DOMAIN|$WP_DOMAIN|" /var/www/html/wordpress/wp-config.php
 sed -i "s|WP_DB_PASSWORD|$MYSQL_WP_PASSWORD|" /var/www/html/wordpress/wp-config.php
 
-php-fpm7 -F -R
+supervisord -c /etc/supervisord.conf
