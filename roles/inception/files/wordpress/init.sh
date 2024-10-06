b789023c8fdbf81c32ec5e13951ef435df68744d
Woolim Park <woolimi91@naver.com>
2024-10-06 23:08:18 +0200

    feat: wordpress who-am-i.html

--- roles/inception/files/wordpress/init.sh
+++ roles/inception/files/wordpress/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sed -i "s|WP_DOMAIN|$WP_DOMAIN|" /var/www/html/wordpress/wp-config.php
 sed -i "s|WP_DB_PASSWORD|$MYSQL_WP_PASSWORD|" /var/www/html/wordpress/wp-config.php
-
+sed -i "s|HOST|$HOST|" /var/www/html/wordpress/who-am-i.html
 supervisord -c /etc/supervisord.conf
