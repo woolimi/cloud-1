0b061a9713c5d60f70dc5cdc526e0e27686bdc2c
Woolim Park <woolimi91@naver.com>
2024-10-07 01:25:17 +0200

    fix: loadbalancer cookie based redirection

--- roles/inception/files/wordpress/init.sh
+++ roles/inception/files/wordpress/init.sh
@@ -2,4 +2,6 @@
 sed -i "s|WP_DOMAIN|$WP_DOMAIN|" /var/www/html/wordpress/wp-config.php
 sed -i "s|WP_DB_PASSWORD|$MYSQL_WP_PASSWORD|" /var/www/html/wordpress/wp-config.php
 sed -i "s|HOST|$HOST|" /var/www/html/wordpress/who-am-i.html
+sed -i "s|PORT|$PORT|" /etc/nginx/nginx.conf
+
 supervisord -c /etc/supervisord.conf
